408db0365126a4db1df5e6165538d44b38fe6929
oussama123-alt <69240644+oussama123-alt@users.noreply.github.com>
2020-08-10 08:57:16 +0100

    aaa

--- resources/views/resp_recrut/resp_recrut_view_candidats.blade.php
+++ resources/views/resp_recrut/resp_recrut_view_candidats.blade.php
@@ -70,7 +70,27 @@
         <td>{{$candidat->email}}</td>
         <td>{{$candidat->phone}}</td>
         <td><a href="{{url('/resp_recrut/'.$candidat->name.'/download')}} "><i class="fa fa-download" aria-hidden="true"></i></a></td>
-        <td><a href="{{url('/resp_recrut/'.$candidat->name)}}"><i class="fa fa-trash"></i></a></td>
+       <td> <a href="" data-toggle="modal" data-target="#myModal"><i class="fa fa-trash" ></i></a></td>
+
+        <!-- Modal pour confirmer la suppression -->                                              
+        <div class="modal fade" id="myModal" role="dialog">
+          <div class="modal-dialog">
+          
+            <!-- Modal content-->
+            <div class="modal-content">
+             
+              <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <p>confirmer la suppression.. </p>
+              </div>
+              <div class="modal-footer">
+                <a href="{{url('/resp_recrut/'.$candidat->name)}}"><i class="fa fa-trash">confirm</i></a>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <td></td>
         </tr>
         @endforeach
       </tbody>
